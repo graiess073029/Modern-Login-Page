8170b153e33fe257d60360f9a1ea2afb91549720
Graiess_Youssef <104838584+graiess073029@users.noreply.github.com>
2022-11-04 15:02:54 +0100

    Update js_tp.js

--- js_tp.js
+++ js_tp.js
@@ -2,12 +2,12 @@ var index = 0;
 
 function security_button0(imgid,input){
     if(index % 2 == 0){
-        imgid.src = "img/unlock-fill.svg"
+        imgid.src = "Modern-Login-Page/unlock-fill.svg"
         input.type = "text"
         index+=1;
     }
     else{
-        imgid.src = "img/lock-fill.svg"
+        imgid.src = "Modern-Login-Page/lock-fill.svg"
         input.type = "password"
 
         index += 1;
